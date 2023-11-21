8d1490b41963fe60a3ad18f3b576919dbf076f28
Walsh99 <146854530+Walsh99@users.noreply.github.com>
2023-11-21 15:43:44 +0000

    done sort and reading file

--- FitnessDataSorter.c
+++ FitnessDataSorter.c
@@ -2,46 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Define the struct for the fitness data
+// Define an appropriate struct
 typedef struct {
-    char date[11];
-    char time[6];
-    int steps;
-} FitnessData;
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+//create an array of type FITNESS_DATA to store the records
+FITNESS_DATA listoffitnessdata[500]; //given up to 500 records
+
+//create temp variables to then move into the typedef array
+char tempdate[11]; //using typedef
+char temptime[6]; //using typedef
+char tempsteps[4]; //should not pass 9999 steps
+
+int linecount = 0; //to count number of lines/records in the csv  
 
 // Function to tokenize a record
-void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
-    char *ptr = strtok(record, &delimiter);
-    if (ptr != NULL) {
-        strcpy(date, ptr);
-        ptr = strtok(NULL, &delimiter);
-        if (ptr != NULL) {
-            strcpy(time, ptr);
-            ptr = strtok(NULL, &delimiter);
-            if (ptr != NULL) {
-                *steps = atoi(ptr);
-            }
-        }
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) 
+    {
+        strcpy(date, token);
     }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) 
+    {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) 
+    {  
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
 }
 
-
+// Complete the main function
 int main() 
 {
-    //create an array of type FITNESS_DATA to store the records
-    FitnessData listoffitnessdata[500]; //given up to 500 records
-
-    //create temp variables to then move into the typedef array
-    char tempdate[11]; //using typedef
-    char temptime[6]; //using typedef
-    int tempsteps; //should not pass 9999 steps
-
-    int linecount = 0; //to count number of lines/records in the csv  
-
-
     //Open file
     char *filename = "FitnessData_2023.csv";
-
     FILE *file = fopen(filename, "r"); //to read
     if (file == NULL)
     {
@@ -53,17 +66,36 @@ int main()
 
     while (fgets(line_buffer, buffer_size, file) != NULL) //loop through the csv record by record
     {
-        tokeniseRecord(line_buffer, ',', tempdate, temptime, *tempsteps); //use the tokeniseRecord to 
+        tokeniseRecord(line_buffer, ",", tempdate, temptime, tempsteps); //use the tokeniseRecord to 
         //copy all the temporary data from the cycle to the array of fitness_data
         strcpy(listoffitnessdata[linecount].date ,tempdate);
         strcpy(listoffitnessdata[linecount].time, temptime);
-        listoffitnessdata[linecount].steps = tempsteps; 
+        listoffitnessdata[linecount].steps = atoi(tempsteps); //had to use atoi to convert the str to int
         
-        printf("%s\n",tempdate);
-        printf("%s\n",temptime);
-        printf("%d\n",tempsteps);
-
-        printf("%s/%s/%d\n",listoffitnessdata[linecount].date, listoffitnessdata[linecount].time, listoffitnessdata[linecount].steps);
         linecount = linecount + 1; 
     }
+    
+    //bubble sort to sort the data by descending order
+    for (int i = 0; i < linecount - 1; i++)
+    {
+        for (int j = 0; j < linecount - 1; j++)
+        {
+            if (listoffitnessdata[j].steps > listoffitnessdata[j + 1].steps)
+            {
+                //swap array[j] and array[j + 1] around
+                FITNESS_DATA temp = listoffitnessdata[j];
+                listoffitnessdata[j] = listoffitnessdata[j + 1];
+                listoffitnessdata[j + 1] = temp;
+            }
+        }
+    }
+    
+    for (int x = 0; x < linecount; x++)
+    {
+        printf("%s\t%s\t%d\n",listoffitnessdata[x].date, listoffitnessdata[x].time, listoffitnessdata[x].steps);
+    }
+    
+
+    fclose(file); //close the file
+    return 0;
 }
